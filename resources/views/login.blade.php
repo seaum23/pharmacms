b5aa19fdec26f13743be0cc4814a56f5c34e1282
seaum23 <seaum333@gmail.com>
2022-12-20 16:17:15 +0600

    First commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pharma CMS</title>
+</head>
+<body>
+    <form action="">
+        <input type="text" name="name" id="name" placeholder="Name">
+        <input type="text" name="phone" id="phone" placeholder="Phone">
+        <input type="text" name="email" id="email" placeholder="Email">
+        <input type="password" name="password" id="password">
+        <button>Submit</button>
+    </form>
+</body>
+</html>
